a61db0b4c67769b7cb6faea2d4c056c63d587a0b
Anish Das <135655078+ragingtitan@users.noreply.github.com>
2024-08-30 06:11:35 +0000

    changed

--- tail-css/src/App.jsx
+++ tail-css/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-extrabold underline text-red-700">
+    <h1 className="text-3xl italic font-extrabold underline text-red-700 bg-black text-center">
       Hello world!
     </h1>
   )
