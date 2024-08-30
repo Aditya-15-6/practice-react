29884703d4f52a21cdcba4eb302cf9e98842fcde
Anish Das <135655078+ragingtitan@users.noreply.github.com>
2024-08-30 06:07:41 +0000

    installed tailwind css

--- tail-css/src/App.jsx
+++ tail-css/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-extrabold underline text-red-700">
       Hello world!
     </h1>
   )
